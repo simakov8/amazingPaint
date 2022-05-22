9da7030bdde5c2a56a344efd0ff5eaa021560c6d
simakov8 <egor.sima.2003@gmail.com>
2022-05-22 16:59:40 +0300

    adding GraphicalTool class

--- Graphics/GraphicalElement.h
+++ Graphics/GraphicalElement.h
@@ -2,15 +2,14 @@
 
 #include <memory>
 #include <QPoint>
-#include "Manipulator.h"
+#include "GraphicalTool.h" 
 #include "Painter.h"
 
-class IGraphicalElement
+class IGraphicalElement : public IGraphicalTool
 {
 public:
 	virtual ~IGraphicalElement() = default;
 
-	virtual std::shared_ptr<IManipulator> GetManipulator() const = 0;
 	virtual std::shared_ptr<IPainter> GetPainter() const = 0;
 
 	// return central point relatively bounding rectangle
