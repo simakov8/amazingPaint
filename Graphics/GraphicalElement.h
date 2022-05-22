02a11f5fde511343d288d94771a0afd45da4529c
simakov8 <egor.sima.2003@gmail.com>
2022-05-22 18:05:30 +0300

    some graphical structure improvements

--- Graphics/GraphicalElement.h
+++ Graphics/GraphicalElement.h
@@ -14,4 +14,9 @@ public:
 
 	// return central point relatively bounding rectangle
 	virtual QPoint GetCentralPoint() const = 0;
+
+	virtual QRect GetBoundingRect() const = 0;
+	virtual void SetBoundingRect(const QRect& boundingRect) = 0;
+
+	virtual	qreal GetTiltAngle() const = 0;
 };
